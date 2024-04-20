e6748f665d27c388956c5b4be88180fcb890a2ba
Shruthi Chinnamsetty <chshruthi07@outlook.com>
2024-04-20 11:21:53 +0100

    more

--- task5.java
+++ task5.java
@@ -1,29 +1,104 @@
-public class task5 {
-    public static int countShips(char[][] board, String shipType, String damageType) {
-        if (shipType!= "Carrier" && shipType!= "Battleship" && shipType!= "Cruiser" && shipType!= "Destroyer") {
-            return 0; //invalid ship type
-        } else if (damageType!= "undamaged" && damageType!= "damaged" && damageType!= "sunk" && damageType!= "all types") {
-            return 2; //invalid damage type
-        }
-        return 0;
-    }
+import java.util.HashMap;
 
-        public static void main(String[] args) {
+public class task5{
 
-            char[][] board = {
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
-                {'.', '.', '.', 'S', 'S', '.', '.', '.', '.', '.'}
-            };
+    public static int countShips(char[][] board, String typeOfShip, String damageTypeOfShip) {
 
-            System.out.println(countShips(board, "Carrier", "sunk"));
+        HashMap<String, Integer> sizesOfShipTypes = new HashMap<>();
 
+        sizesOfShipTypes.put("Carrier", 5);
+        sizesOfShipTypes.put("Battleship", 4);
+        sizesOfShipTypes.put("Cruiser", 3);
+        sizesOfShipTypes.put("Destroyer", 2);
+
+        boolean validTypeOfShip;
+        if (typeOfShip!= "Carrier" && typeOfShip!= "Battleship" && typeOfShip!= "Cruiser" && typeOfShip!= "Destroyer"){
+            validTypeOfShip = false;
+
+        } else {
+            validTypeOfShip = true;
         }
 
+        if (validTypeOfShip == false) {
+            return -1;
+        }
+
+        boolean validDamageTypeOfShip;
+
+        if(damageTypeOfShip!= "undamaged" && damageTypeOfShip!= "damaged" && damageTypeOfShip!= "sunk" && damageTypeOfShip!= "all types"){
+            validDamageTypeOfShip = false;
+        } 
+        else {
+            validDamageTypeOfShip = true;
+        }
+
+        if (validDamageTypeOfShip == false) {
+            return -2;
+        }
+        
+        int sunkShips = 0;
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == 'S' || board[i][j] == '*') {
+                    int shipLength = sizesOfShipTypes.get(typeOfShip);
+                    boolean isPartOfShip = true;
+                    for (int k = 1; k < shipLength; k++) {
+                        if (j + k >= board[0].length || board[i][j + k] != 'S') {
+                            isPartOfShip = false;
+                            break;
+                        }
+                    }
+                    if (isPartOfShip) {
+                        switch (damageTypeOfShip) {
+                            case "undamaged":
+                                if (board[i][j] == 'S') {
+                                    sunkShips += 1;
+                                }
+                                break;
+                            case "damaged":
+                                if (board[i][j] == '*') {
+                                    sunkShips += 1;
+                                }
+                                break;
+                            case "sunk":
+                                boolean isSunk = true;
+                                for (int k = 0; k < shipLength; k++) {
+                                    if (board[i][j + k] != '*') {
+                                        isSunk = false;
+                                        break;
+                                    }
+                                }
+                                if (isSunk) {
+                                    sunkShips += 1;
+                                }
+                                break;
+                            case "all types":
+                                sunkShips += 1;
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return sunkShips;
     }
+    
+    public static void main(String[] args) {
+        char[][] board = {
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', 'S', 'S', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', 'S', 'S', 'S', 'S', '*', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'},
+            {'.', '.', '.', 'S', 'S', 'S', 'S', 'S', '.', '.'},
+            {'.', '.', '.', '.', '.', '.', '.', '.', '.', '.'}
+        };
+        
+        System.out.println("Number of undamaged carriers: " + countShips(board, "Cruiser", "all types"));
+    }
+}
